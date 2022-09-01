801a9038f0e473b962687a6ad6fdb8fde80ebdbe
rswetnam <rswetnam@Check30.onmicrosoft.com>
2022-08-31 20:27:20 -0400

    Add project files.

---
+++ Globoticket.Web/ColorHtmlLocalizer.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
+
+namespace Globoticket.Web
+{
+    public class ColorHtmlLocalizer : IHtmlLocalizer
+    {
+        public LocalizedHtmlString this[string name] => 
+            new LocalizedHtmlString(name, $"<span style='color: #{RandomColor}'>{name}</span>");
+
+        public LocalizedHtmlString this[string name, params object[] arguments]
+        {
+            get
+            {
+                var value = string.Format(name, arguments);
+                return new LocalizedHtmlString(name, $"<span style='color: #{RandomColor}'>{value}</span>");
+            }
+        } 
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LocalizedString GetString(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LocalizedString GetString(string name, params object[] arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string RandomColor => new Random().Next(0, 16777216).ToString("x6");
+    }
+}
---
+++ Globoticket.Web/Program.cs
@@ -0,0 +1,43 @@
+using Globoticket.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[] { "en", "de", "es", "fr" };
+    options
+        .AddSupportedCultures(supportedCultures)
+        .AddSupportedUICultures(supportedCultures)
+        .SetDefaultCulture("en");
+});
+
+builder.Services.AddLocalization(
+    options => options.ResourcesPath = "Resources");
+
+builder.Services.AddSingleton<ColorHtmlLocalizer>();
+
+builder.Services.AddRazorPages().AddViewLocalization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseRequestLocalization();
+
+app.MapRazorPages();
+
+app.Run();
